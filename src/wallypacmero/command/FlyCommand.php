778a38c2d2045da5e93d172e0ce23e1a8ab8ecdb
wally-pacmero <74281685+wally-pacmero@users.noreply.github.com>
2022-09-03 20:22:45 -0500

    Update FlyCommand.php

--- src/wallypacmero/command/FlyCommand.php
+++ src/wallypacmero/command/FlyCommand.php
@@ -6,6 +6,7 @@ use pocketmine\command\CommandSender;
 use pocketmine\command\Command;
 use pocketmine\player\Player;
 use pocketmine\utils\TextFormat;
+use pocketmine\event\player\PlayerJoinEvent;
 
 class FlyCommand extends Command
 {
@@ -13,6 +14,16 @@ class FlyCommand extends Command
     public function __construct()
     {
         parent::__construct("fly", "Fly command", "/fly", ["f"]);
+    }
+
+    public function onPlayerJoin(PlayerJoinEvent $event)
+    {
+        $player = $event->getPlayer();
+        if($player->getAllowFlight()) {
+            $player->setFlying(false);
+            $player->setAllowFlight(false);
+            $player->sendMessage(TextFormat::RED . "Your flight has been disabled");
+        }
     }
 
     public function execute(CommandSender $sender, string $commandLabel, array $args): bool
