c0edcc1f5376867a7d80a5a04e38865e702f77a0
wally-pacmero <74281685+wally-pacmero@users.noreply.github.com>
2022-09-27 10:40:53 -0500

    Update Loader.php

--- src/wallypacmero/Loader.php
+++ src/wallypacmero/Loader.php
@@ -9,11 +9,11 @@ use wallypacmero\command\FlyCommand;
 
 class Loader extends PluginBase
 {
- public function onEnable(): void
- {
-  $this->getServer()->getCommandMap()->register("fly", new FlyCommand($this));
-  $this->getLogger()->info("Enabled");
- }
+    public function onEnable(): void
+    {
+       $this->getServer()->getCommandMap()->register("fly", new FlyCommand());
+       $this->getLogger()->info("Enabled");
+    }
     public function onPlayerJoin(PlayerJoinEvent $event)
     {
         $player = $event->getPlayer();
