72834b4b3b1e5a6d6a7c5dbc41323b8d59588895
wally-pacmero <74281685+wally-pacmero@users.noreply.github.com>
2022-09-27 10:35:38 -0500

    Update Loader.php

--- src/wallypacmero/Loader.php
+++ src/wallypacmero/Loader.php
@@ -2,7 +2,9 @@
 
 namespace wallypacmero;
 
+use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat;
 use wallypacmero\command\FlyCommand;
 
 class Loader extends PluginBase
@@ -12,4 +14,13 @@ class Loader extends PluginBase
   $this->getServer()->getCommandMap()->register("fly", new FlyCommand($this));
   $this->getLogger()->info("Enabled");
  }
+    public function onPlayerJoin(PlayerJoinEvent $event)
+    {
+        $player = $event->getPlayer();
+        if($player->getAllowFlight()) {
+            $player->setFlying(false);
+            $player->setAllowFlight(false);
+            $player->sendMessage(TextFormat::RED . "Your flight has been disabled");
+        }
+    }
 }
